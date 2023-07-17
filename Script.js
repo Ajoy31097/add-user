ba96e1f9c265fcfac99a959599a6413f61bebc23
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-07-17 21:46:07 +0530

    DELETE request using axios

--- Script.js
+++ Script.js
@@ -1,14 +1,3 @@
-// function myfunction(event) {
-//   event.preventDefault();
-
-//   var user_details = {
-//     Name: document.getElementById('name').value,
-//     Email: document.getElementById('email').value,
-//     Phone_No: document.getElementById('phone').value
-//   };
-
-  
-
 window.onload = function() {
   axios.get('https://crudcrud.com/api/390c532274d344838cf2c93bce04747e/user_details')
     .then(function(response) {
@@ -24,6 +13,7 @@ window.onload = function() {
 
 function ShowUserOnScreen(user_details) {
   var userDiv = document.createElement('div');
+  
   var nameParagraph = document.createElement('p');
   nameParagraph.textContent = 'Name: ' + user_details.Name;
   
@@ -33,10 +23,31 @@ function ShowUserOnScreen(user_details) {
   var phoneParagraph = document.createElement('p');
   phoneParagraph.textContent = 'Phone Number: ' + user_details.Phone_No;
   
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', function() {
+    event.stopPropagation();
+    DeleteUser(user_details._id, userDiv);
+  });
+  
   userDiv.appendChild(nameParagraph);
   userDiv.appendChild(emailParagraph);
   userDiv.appendChild(phoneParagraph);
+  userDiv.appendChild(deleteButton);
   
   var outputContainer = document.getElementById('output');
   outputContainer.appendChild(userDiv);
+}
+
+function DeleteUser(userId, userDiv) {
+  axios.delete(`https://crudcrud.com/api/390c532274d344838cf2c93bce04747e/user_details/${userId}`)
+    .then(function(response) {
+      console.log(response.data);
+      if (response.status === 200) {
+        userDiv.remove();
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
 }
